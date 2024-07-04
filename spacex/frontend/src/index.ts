008ea187336302f4c1fbcd820d9a2ee72629faf9
Tryd0g0lik <work80@mail.ru>
2024-07-04 19:17:30 +0700

    [1]: The end

--- spacex/frontend/src/index.ts
+++ spacex/frontend/src/index.ts
@@ -1,3 +1,3 @@
 import './App.tsx'// './scripts/index' 
-
+import './javascripts.ts'
 
--- spacex/models.py
+++ spacex/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.core.validators import RegexValidator, MaxLengthValidator, MinLengthValidator
 
 
-# Create your models here.
-
 class MenuItemModels(models.Model):
 	title = models.CharField(
 		max_length=20,
